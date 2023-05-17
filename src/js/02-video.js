2c5e1d5ccb5adc84e5af4a7b3130586fd3ca1b27
TaniaMateichuk <tania.0309.tsyra@gmail.com>
2023-05-17 03:11:55 +0300

    Task 1 and 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// Ознайомся з документацією бібліотеки Vimeo плеєра.
+// Додай бібліотеку як залежність проекту через npm.
+// Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const timePause = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(timePause, function () {
+  if (timePause === null) {
+    player.setCurrentTime(0);
+  }
+  return;
+});
+console.log(timePause);
+
+player.on('timeupdate', throttle(onSavePause, 1000));
+
+
+
+function onSavePause({ seconds } = 0) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+}
